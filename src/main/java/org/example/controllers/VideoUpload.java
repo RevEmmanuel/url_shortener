b848e4a5fdf7ccb3be3595bcfd99d92f5b980e84
RevEmmanuel <adeolaae1@gmail.com>
2023-10-01 23:43:25 +0100

    videos

--- src/main/java/org/example/controllers/VideoUpload.java
+++ src/main/java/org/example/controllers/VideoUpload.java
@@ -1,7 +1,9 @@
 package org.example.controllers;
 
 import org.example.data.models.KhalidVideo;
+import org.example.data.models.TifeVideo;
 import org.example.data.repositories.KhalidVideoRepository;
+import org.example.data.repositories.TifeVideoRepository;
 import org.example.exceptions.UrlShortenerException;
 import org.example.services.cloud.CloudService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/api/v1/video")
 public class VideoUpload {
@@ -19,6 +23,8 @@ public class VideoUpload {
     private CloudService cloudService;
     @Autowired
     private KhalidVideoRepository khalidVideoRepository;
+    @Autowired
+    private TifeVideoRepository tifeVideoRepository;
 
 
     @PostMapping(value = "/upload/{name}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
@@ -37,6 +43,27 @@ public class VideoUpload {
         try {
             KhalidVideo khalidVideo = khalidVideoRepository.findByName(name);
             return new ResponseEntity<>(khalidVideo.getVideoUrl(), HttpStatus.OK);
+        }catch (UrlShortenerException exception){
+            return ResponseEntity.badRequest().body(exception.getMessage());
+        }
+    }
+
+    @PostMapping(value = "/upload-video/{userId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> uploadVideo(@RequestParam(value = "file") MultipartFile file, @PathVariable String userId) {
+        try {
+            String response = cloudService.upload(file);
+            tifeVideoRepository.save(TifeVideo.builder().uniqueUserId(userId).videoUrl(response).build());
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        }catch (UrlShortenerException exception){
+            return ResponseEntity.badRequest().body(exception.getMessage());
+        }
+    }
+
+    @GetMapping(value = "/all/{userId}")
+    public ResponseEntity<?> getVideosUrl(@PathVariable String userId) {
+        try {
+            List<TifeVideo> tifeVideo = tifeVideoRepository.findAllByUniqueUserId(userId);
+            return new ResponseEntity<>(tifeVideo, HttpStatus.OK);
         }catch (UrlShortenerException exception){
             return ResponseEntity.badRequest().body(exception.getMessage());
         }
